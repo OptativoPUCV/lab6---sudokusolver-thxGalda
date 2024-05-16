f311c10fca49ea01c9ea2cb95c7007b33d96b985
Your Name <you@example.com>
2024-05-16 19:37:22 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -1,109 +1,100 @@
+#include "list.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
 
+typedef struct {
+  int sudo[9][9];
+} Node;
 
-typedef struct{
-   int sudo[9][9];
-}Node;
-
-Node* createNode(){
-  Node* n=(Node*) malloc(sizeof(Node));
+Node *createNode() {
+  Node *n = (Node *)malloc(sizeof(Node));
   return n;
 }
 
-Node* copy(Node* n){
-    Node* new=(Node*) malloc(sizeof(Node));
-    *new = *n;
-    return new;
+Node *copy(Node *n) {
+  Node *new = (Node *)malloc(sizeof(Node));
+  *new = *n;
+  return new;
 }
 
-Node* read_file (char* file_name){
-  Node* n = createNode();
-  FILE* file = fopen (file_name, "r");
-  int i,j;
-  for(i=0;i<9;i++){
-       for(j=0;j<9;j++){
-          if(!fscanf (file, "%d", &n->sudo[i][j]))
-            printf("failed to read data!");
-       }
+Node *read_file(char *file_name) {
+  Node *n = createNode();
+  FILE *file = fopen(file_name, "r");
+  int i, j;
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++) {
+      if (!fscanf(file, "%d", &n->sudo[i][j]))
+        printf("failed to read data!");
+    }
   }
 
-  fclose (file);
+  fclose(file);
   return n;
 }
 
-void print_node(Node* n){
-    int i,j;
-    for(i=0;i<9;i++){
-       for(j=0;j<9;j++)
-          printf("%d ", n->sudo[i][j]);
-       printf("\n");
-    }
+void print_node(Node *n) {
+  int i, j;
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++)
+      printf("%d ", n->sudo[i][j]);
     printf("\n");
+  }
+  printf("\n");
 }
 
-int is_valid(Node* n){
-   int i,j;
-   for(i = 0; i < 9; i++){
-      int valid[10] = {0,0,0,0,0,0,0,0,0,0};
-      for(j = 0; j < 9; j++){
-         if(n->sudo[i][j] != 0){
-            if(valid[n->sudo[i][j]] == 0){
-               valid[n->sudo[i][j]] = 1;
-            }
-            else return 0;
-         }
+int is_valid(Node *n) {
+  int i, j;
+  for (i = 0; i < 9; i++) {
+    int valid[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    for (j = 0; j < 9; j++) {
+      if (n->sudo[i][j] != 0) {
+        if (valid[n->sudo[i][j]] == 0) {
+          valid[n->sudo[i][j]] = 1;
+        } else
+          return 0;
       }
-   }
+    }
+  }
 
-   for(i = 0; i < 9; i++){
-      int valid[10] = {0,0,0,0,0,0,0,0,0,0};
-      for(j = 0; j < 9; j++){
-         if(n->sudo[j][i] != 0){
-            if(valid[n->sudo[j][i]] == 0){
-               valid[n->sudo[j][i]] = 1;
-            }
-            else return 0;
-         }
+  for (i = 0; i < 9; i++) {
+    int valid[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    for (j = 0; j < 9; j++) {
+      if (n->sudo[j][i] != 0) {
+        if (valid[n->sudo[j][i]] == 0) {
+          valid[n->sudo[j][i]] = 1;
+        } else
+          return 0;
       }
-   }
+    }
+  }
 
-   for(int block = 0; block < 9; block++){
-      int valid[10] = {0,0,0,0,0,0,0,0,0,0};
-      for(i = 0; i < 3; i++){
-         for(j = 0; j < 3; j++){
-            int x = (block / 3) * 3 + i;
-            int y = (block % 3) * 3 + j;
-            if(n->sudo[x][y] != 0){
-               if(valid[n->sudo[x][y]] == 0){
-                  valid[n->sudo[x][y]] = 1;
-               }
-               else return 0;
-            }
-         }
+  for (int block = 0; block < 9; block++) {
+    int valid[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    for (i = 0; i < 3; i++) {
+      for (j = 0; j < 3; j++) {
+        int x = (block / 3) * 3 + i;
+        int y = (block % 3) * 3 + j;
+        if (n->sudo[x][y] != 0) {
+          if (valid[n->sudo[x][y]] == 0) {
+            valid[n->sudo[x][y]] = 1;
+          } else
+            return 0;
+        }
       }
-   }
-   return 1;
+    }
+  }
+  return 1;
 }
 
-
-
-List* get_adj_nodes(Node* n){
-    List* list=createList();
-    return list;
+List *get_adj_nodes(Node *n) {
+  List *list = createList();
+  int i, j;
+  return list;
 }
 
+int is_final(Node *n) { return 0; }
 
-int is_final(Node* n){
-    return 0;
-}
-
-Node* DFS(Node* initial, int* cont){
-  return NULL;
-}
-
-
+Node *DFS(Node *initial, int *cont) { return NULL; }
 
 /*
 int main( int argc, char *argv[] ){
